2406c64c3e416aaadd291f3be329b92a5c371bee
Dominic Boggio <dominicboggio@gmail.com>
2020-07-25 02:02:35 -0400

    grid works now

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
 const gridSize = 16;
 var container = document.querySelector('.container')
-for(var i = 0; i < gridSize; i++)
+function createGrid(size)
 {
-    var row = document.createElement("div");
-    row.id = "row";
-    for(var j = 0; j < gridSize; j++)
+    console.log(size);
+    for(var i = 0; i < size; i++)
     {
-        var cell = document.createElement("div");
-        cell.id = "cell";
-        row.appendChild(cell);
+        var row = document.createElement("div");
+        row.id = "row";
+        for(var j = 0; j < size; j++)
+        {
+            var cell = document.createElement("div");
+           
+            cell.id = "cell";
+            row.appendChild(cell);
+            
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
 }
+createGrid(gridSize);
+var cells;
+addCellListener();
+function addCellListener()
+{
+    cells = document.querySelectorAll('#cell');
+    cells.forEach((c) =>{
+        c.addEventListener('mouseover', () =>{
+            c.style = "background-color: black;";
+        });
+    });
+}
+function clearGrid()
+{
+    cells.forEach((c) =>{
+        c.remove();
+    });
+    var newGridSize = prompt("Enter a new grid size!");
+    if(newGridSize == NaN)
+    {
+        createGrid(16);
+    }else
+    {
+        createGrid(parseInt(newGridSize));
+    }
+    addCellListener();
+} 
